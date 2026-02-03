6984d1fffdb305a3d424c9c19e202bb06f3d3cb6
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2026-02-03 14:22:55 +0530

    Updated db.loader.ts - Made safe for retries

--- backend/src/loaders/db.loader.ts
+++ backend/src/loaders/db.loader.ts
@@ -3,8 +3,12 @@ import { logger } from "../config/logger.config";
 
 export const initDB = async (): Promise<void> => {
   try {
-    await JobPortalDataSource.initialize();
-    logger.info("Database connected");
+    if (!JobPortalDataSource.isInitialized) {
+      await JobPortalDataSource.initialize();
+      logger.info("Database connected");
+    } else {
+      logger.warn("Database already initialized");
+    }
   } catch (error) {
     logger.error("Database connection failed", error);
     process.exit(1);
