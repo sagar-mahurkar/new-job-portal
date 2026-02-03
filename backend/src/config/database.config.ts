e469cbb037d503f0a314979b75395f4134f551d9
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2026-02-03 12:16:28 +0530

    Updated database.config.ts - database config is created

---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,22 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import { env } from "./env.config";
+
+
+// Purpose
+// Create DB config
+const databaseConfig: DataSourceOptions = {
+  type: "postgres",
+  host: env.POSTGRES_HOST,
+  port: env.POSTGRES_PORT,
+  username: env.POSTGRES_USERNAME,
+  password: env.POSTGRES_PASSWORD,
+  database: env.POSTGRES_DATABASE,
+  synchronize: false,
+  logging: env.NODE_ENV !== "preprod",
+  entities: [__dirname + "/../modules/**/*.entity.{ts,js}"],
+  migrations: [__dirname + "/migrations/*.{ts,js}"],
+  migrationsRun: false,
+};
+
+// Export DataSource / Prisma client
+export const JobPortalDataSource = new DataSource(databaseConfig);
