9903a0845e6f9ffa7d99eece5bf8502641029c1b
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2026-02-09 16:25:56 +0530

    feat(error): handle zod validation and database query errors globally

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/common/errors/AppError";
 import { logger } from "@/config/logger.config";
+import { ZodError } from "zod";
+import { HttpStatusCodes } from "@/common/constants/http.codes";
+import { QueryFailedError } from "typeorm";
 
 export const errorMiddleware = (
   err: Error,
@@ -8,7 +11,26 @@ export const errorMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  // Operational errors (expected)
+  // 1. Zod validation errors
+  if (err instanceof ZodError) {
+    logger.warn("Validation error", {
+      statusCode: HttpStatusCodes.BAD_REQUEST,
+      path: req.originalUrl,
+      method: req.method,
+      errors: err.issues      
+    });
+
+    return res.status(HttpStatusCodes.BAD_REQUEST).json({
+      success: false,
+      message: "Validation failed",
+      errors: err.issues.map(e => ({
+        field: e.path.join("."),
+        message: e.message
+      }))
+    })
+  }
+
+  // 2. Operational errors (expected)
   if (err instanceof AppError && err.isOperational) {
     logger.warn(err.message, {
       statusCode: err.statusCode,
@@ -22,7 +44,21 @@ export const errorMiddleware = (
     });
   }
 
-  // Unknown / programmer / system errors
+  // 3. Postgres Errors (unexpected)
+  if (err instanceof QueryFailedError) {
+    logger.error("Database error", {
+      message: err.message,
+      query: err.query,
+    });
+
+    return res.status(HttpStatusCodes.BAD_REQUEST).json({
+      success: false,
+      message: "Database operation failed",
+    });
+  }
+
+
+  // 4. Unknown / programmer / system errors
   logger.error("Unhandled error", {
     error: err,
     path: req.originalUrl,
