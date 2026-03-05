e2965a63e595a1be6b0d7872dbd1e3e3f8eb62e3
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2026-03-05 09:42:34 +0530

    feat(frontend): add QueryProvider and integrate React Query

---
+++ frontend/src/app/providers/QueryProvider.tsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
+
+const queryClient = new QueryClient();
+
+type QueryProviderProps = {
+  children: ReactNode
+};
+
+function QueryProvider({children}: QueryProviderProps) {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </>
+  )
+}
+
+export { QueryProvider }
